ea6acc71de9507c68ce79e38438a0c00b8849348
PeterYang12 <yuhan.yang@intel.com>
2022-05-29 17:49:18 +0800

    Template

---
+++ practise/kbd.c
@@ -0,0 +1,5 @@
+#include "kbd.h"
+void kbd(){
+    int i=0;
+    printf("i: %d\n",i);
+}
---
+++ practise/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "kbd.h"
+int main(){
+    kbd();
+    printf("main\n");
+    return 0;
+}
